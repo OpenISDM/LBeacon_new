00e9a44f40c900f99618fdbe9dab32a70f0df0d7
chunyu1202 <chunyu1202@gmail.com>
2018-12-28 17:31:40 +0800

    Sync thpool codes between LBeacon and Gateway

--- src/thpool.c
+++ src/thpool.c
@@ -22,6 +22,8 @@ Memory_Pool th_mempool;
 /* Initialise thread pool */
 struct thpool_* thpool_init(int num_threads){
 
+	int return_value;
+
 	threads_on_hold   = 0;
 	threads_keepalive = 1;
 
@@ -30,11 +32,9 @@ struct thpool_* thpool_init(int num_threads){
 	}
 
     /* Initialize the memory pool */
-    if(MEMORY_POOL_SUCCESS != 
-	mp_init(&th_mempool, SIZE_FOR_MEM_POOL, SLOTS_FOR_MEM_POOL)){
-
+    if(mp_init(&th_mempool, SIZE_FOR_MEM_POOL, SLOTS_FOR_MEM_POOL)
+       != MEMORY_POOL_SUCCESS)
         return NULL;
-    }
 
 	/* Make new thread pool */
 	thpool_* thpool_p;
@@ -229,7 +229,7 @@ static void* thread_do(struct thread* thread_p){
 	sprintf(thread_name, "thread-pool-%d", thread_p->id);
 
 
-	pthread_setname_np(thread_name);
+	pthread_setname_np(thread_p->pthread, thread_name);
 
 	/* Assure all threads have been created before starting serving */
 	thpool_* thpool_p = thread_p->thpool_p;
@@ -265,7 +265,7 @@ static void* thread_do(struct thread* thread_p){
 			if (job_p) {
 				func_buff = job_p->function;
 				arg_buff  = job_p->arg;
-				pthread_setschedprio(thread_p->pthread, job_p->priority);
+				setpriority(PRIO_PROCESS, thread_p->pthread, job_p->priority);
 				func_buff(arg_buff);
 				mp_free(&th_mempool ,job_p);
 			}
@@ -291,9 +291,6 @@ static void* thread_do(struct thread* thread_p){
 static void thread_destroy (thread* thread_p){
 	mp_free(&th_mempool, thread_p);
 }
-
-
-
 
 
 /* ============================ JOB QUEUE =========================== */
@@ -358,12 +355,10 @@ static void jobqueue_push(jobqueue* jobqueue_p, struct job* newjob){
 }
 
 
-/* Get first job from queue(removes it from queue)
-<<<<<<< HEAD
+/*
+ * Get first job from queue(removes it from queue)
  *
  * Notice: Caller MUST hold a mutex
-=======
->>>>>>> da2c0fe45e43ce0937f272c8cd2704bdc0afb490
  */
 static struct job* jobqueue_pull(jobqueue* jobqueue_p){
 
@@ -399,9 +394,6 @@ static void jobqueue_destroy(jobqueue* jobqueue_p){
 	jobqueue_clear(jobqueue_p);
 	mp_free(&th_mempool, jobqueue_p->has_jobs);
 }
-
-
-
 
 
 /* ======================== SYNCHRONISATION ========================= */
--- src/thpool.h
+++ src/thpool.h
@@ -3,10 +3,12 @@
  * License:     MIT
  *
  **********************************/
+
 #ifndef THPOOL_H
 #define THPOOL_H
 
-#define _POSIX_C_SOURCE 200809L
+#define _GNU_SOURCE
+
 #include <unistd.h>
 #include <signal.h>
 #include <stdio.h>
@@ -14,15 +16,22 @@
 #include <pthread.h>
 #include <errno.h>
 #include <time.h>
+#include <sys/time.h>
+#include <sys/resource.h>
 #include "Mempool.h"
 
+
+/* The number of slots for the memory pool */
+#define SLOTS_FOR_MEM_POOL 100
+
+/* The number of slots for the memory pool */
+#define SIZE_FOR_MEM_POOL 256
 
 #define err(str) fprintf(stderr, str)
 
 
 static volatile int threads_keepalive;
 static volatile int threads_on_hold;
-
 
 
 /* ========================== STRUCTURES ============================ */
@@ -72,11 +81,6 @@ typedef struct thpool_{
 	pthread_cond_t  threads_all_idle;    /* signal to thpool_wait     */
 	jobqueue  jobqueue;                  /* job queue                 */
 } thpool_;
-
-
-
-
-
 
 
 /* ========================== PROTOTYPES ============================ */
@@ -269,11 +273,4 @@ void thpool_destroy(Threadpool);
  */
 int thpool_num_threads_working(Threadpool);
 
-/* The number of slots for the memory pool */
-#define SLOTS_FOR_MEM_POOL 100
-
-/* The number of slots for the memory pool */
-#define SIZE_FOR_MEM_POOL 256
-
 #endif
-
