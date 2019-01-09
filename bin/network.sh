43ab5ff899d36d1e9ff0f2948bfe8824dd4eb98d
Gary Hsiao <26973371+garyh0205@users.noreply.github.com>
2019-01-09 14:01:56 +0800

    Fix reconnection fail.

--- bin/network.sh
+++ bin/network.sh
@@ -3,6 +3,7 @@
 #check if we could ping to routers (Gateway or AP)
 sudo cat /etc/dhcpcd.conf | grep "routers" | cut -d "=" -f 2 | xargs ping -nc 1
 if [ "X$?" != "X0" ]; then
-    sudo systemctl daemon-reload
-    sudo service dhcpcd restart
+    sudo ifdown wlan0
+    sleep 5
+    sudo ifup wlan0
 fi
