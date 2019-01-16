9a63ff739d056b939f174b0bae37e993a666bf67
chunyu1202 <chunyu1202@gmail.com>
2019-01-16 17:18:31 +0800

    Change the method to re-establish network when network is not connected.

--- bin/network.sh
+++ bin/network.sh
@@ -3,6 +3,6 @@
 #check if we could ping to routers (Gateway or AP)
 sudo cat /etc/dhcpcd.conf | grep "routers" | cut -d "=" -f 2 | xargs ping -nc 1
 if [ "X$?" != "X0" ]; then
-    sudo systemctl daemon-reload
-    sudo service dhcpcd restart
+    sudo ifdown --force wlan0
+    sudo ifup wlan0
 fi
